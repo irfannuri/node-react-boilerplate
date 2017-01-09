d3b5bcc590c0694bd91ba5c5fd81138066d65fcc
İrfan Nuri Karaca <irfan@Irfans-MacBook-Pro.local>
2017-01-09 15:23:12 +0300

    es6 block scoped decleration fix

--- spec/services/serviceProvider.spec.js
+++ spec/services/serviceProvider.spec.js
@@ -1,12 +1,11 @@
 /**
  * Created by irfan on 04/01/2017.
  */
-"use strict";
 
 describe("Service Provider", function(){
 
   it("getBasicService should return a Basic Service", function(){
-
+    "use strict";
     let BasicService = require('services/basicService/basicService');
     let service = require("services/basicService/serviceProvider").getBasicService();
     expect(service).toEqual(jasmine.any(BasicService));
